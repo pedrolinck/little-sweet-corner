f73b3aee59df3b1f9aec1f2c1385b24cb205323d
pedro_linck@outlook.com <pedro_linck@outlook.com>
2025-01-04 23:00:48 -0300

    create logic to add a list item from button click

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-let data = document.getElementById("data")
+const btnCard = document.querySelectorAll('button');
+const imgCard = document.getElementsByClassName('imgCard');
+const payTitle = document.querySelector("h3.payTitle")
+const emptyCart = document.querySelector(".emptyCart")
 
-fetch("data.json").then((response) => {
-  response.json().then((data) =>{
-    console.log(data);
-  })
+// access data.json
+let data = document.getElementById("data")
+fetch("data.json").then((response) => { response.json().then((data) =>{
+  for(let i = 0; i < data.length; i++){
+    // console.log(data[i].name);
+    
+  }
+  
+})
 })
 
+// function to add order to list 
+function addOrder(){
+  let countText = document.getElementById('count')
+  let count = 0;
 
-const btnCard = document.getElementsByClassName('button');
-const imgCard = document.getElementsByClassName('imgCard');
+  for (let i = 0; i < btnCard.length; i++) {
+
+    const li = document.createElement('li');
+
+    btnCard[i].addEventListener('click', function (){
+      count++;
+      countText.innerHTML = count;
+      emptyCart.innerHTML = ''
+      
+    })
+  }
+}addOrder()
+
+
+// set up event on all buttons
+function setupButton(){
+  btnCard.forEach((button) => {
+    button.addEventListener('click', function(){
+      console.log('button clicked', this);
+      console.log('index', Array.from(btnCard).indexOf(this));
+      
+      
+    })
+  })
+}setupButton()
