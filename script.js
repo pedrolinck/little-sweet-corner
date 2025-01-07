b8a3199056174353a6eae7b7b9784f53e835233a
pedro_linck@outlook.com <pedro_linck@outlook.com>
2025-01-07 19:17:32 -0300

    create structure to add an item, update with a counter if the item was chosed and button section to confirm order

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 (async function(){
-  const btnCard = document.querySelectorAll('button');
-  const emptyCart = document.querySelector(".emptyCart")
-  let products = [];
+  "use strict"
 
+  const btnCard = document.querySelectorAll('.buttonCard');
+  const emptyCart = document.querySelector(".emptyCart")
+  const cardTitle = document.querySelector('.payTitle')
+  const svgImg = document.querySelector('.emptyCart svg')
+  const emptyTitle = document.querySelector('.emptyTitle')
+  let products = [];
+  // object to store product counters
+  const counter = {};
+  // object to store all purchased items 
+  const carItems = {}
+  // store sum of order
+  let totalOrder = 0;
+
+  // div to show total and confirm button dinamically
+  const totalDiv = document.createElement('div')
+  totalDiv.className = 'orderContainer'
+  totalDiv.style.display = "none";
+  totalDiv.innerHTML = `
+    <p class='orderText'>Total order<span class='amount'>$46.00</span></p>
+    <button class='confirmBtn'>Confirm order</button>
+  `
+  // add after list items
+  emptyCart.parentElement.appendChild(totalDiv)
+
+  // select elements inside dynamic div
+  const orderText = document.querySelector('.orderText .amount')
+  const confirmBtn = totalDiv.querySelector('.confirmPurchase')
+
+  // load products from json
   async function loadProduct(){
     const URL = 'http://127.0.0.1:5500/data/data.json'
 
@@ -19,39 +46,84 @@
 
   await loadProduct();
 
+  // add event click on each button
   btnCard.forEach(button => {
     button.addEventListener('click', () => {
-    const countText = document.querySelector('.count')
-    const buttontId = button.getAttribute('data-id');
-    const cardTitle = document.querySelector('.payTitle')
-    let counter = {};
-    
-    
-      cardTitle.innerHTML = `Your Cart (${counter})`
-      countText.innerHTML = counter++;
-    
-    const selectedProduct = products.find(product => product.id == buttontId)  
-    
-    if(selectedProduct){
-      let listItem = document.createElement('li')
-      let sumOrder = (selectedProduct.price * counter).toFixed(2);
-      listItem.innerHTML = `
-        <p>${selectedProduct.name}</p>
-        <span>${counter}x</span> ${selectedProduct.price.toFixed(2)} ${sumOrder}  
-      `
-      emptyCart.innerHTML = ''
-      emptyCart.appendChild(listItem)
-    }else{
-      let listItem = document.createElement('li')
-      let sumOrder = (selectedProduct.price * counter).toFixed(2);
-      listItem.innerHTML += `
-        <p>${selectedProduct.name}</p>
-        <span>${counter}x</span> ${selectedProduct.price.toFixed(2)} ${sumOrder}  
-      `
-      emptyCart.innerHTML = ''
+      const buttontId = button.getAttribute('data-id');
 
-      emptyCart.appendChild(listItem)
-    }
+
+      if(!counter[buttontId]){
+        // initialize product's counter
+        counter[buttontId] = 0
+      }
+
+      counter[buttontId]++;
+
+      // updates the shopping cart
+      const totalItems = Object.values(counter).reduce((sum, count) => sum + count, 0)
+      cardTitle.innerHTML = `Your Cart (${totalItems})`
+
+      // searches especific product
+      const selectedProduct = products.find(product => product.id == buttontId)  
+
+      if(totalItems === 1){
+        if(svgImg) svgImg.remove()
+        if(emptyTitle) emptyTitle.remove()
+      }
+      
+      if(selectedProduct){
+        let listItem;
+        if(!carItems[buttontId]){
+          // if item still isn't on shopping cart, then create a new element
+          listItem = document.createElement('li')
+          listItem.className = 'list-item'
+          listItem.setAttribute('data-id', buttontId)
+          carItems[buttontId] = listItem;
+          emptyCart.appendChild(listItem)
+        }else{
+          // if already is or exist updates the existent element
+          listItem = carItems[buttontId]
+        }
+        
+        let sumItem = (selectedProduct.price * counter[buttontId]).toFixed(2)
+
+        // updates content of shopping cart item
+        listItem.innerHTML = `
+          <p>${selectedProduct.name}</p>
+          <span class='itemData'>
+            <span class='itemContainer'>
+              <span class='itemQtd'>${counter[buttontId]}x</span>
+              ${selectedProduct.price.toFixed(2)} 
+              <span class='totalItem'>$${sumItem}</span>
+            </span>
+            <span class='cancelItem'>x</span>
+          </span>
+          
+        `
+        // update total
+        totalOrder = Object.entries(counter).reduce((total, [id, count]) =>{
+          const product = products.find((p) => p.id == id)
+          return total + product.price * count
+        })
+
+        // orderText.innerHTML = `Total order $${totalOrder.toFixed(2)}`;
+        totalDiv.style.display = "block";
+      }
+
+      // confirm purchase
+      confirmBtn.addEventListener("click", () => {
+        alert(`Order confirmed! Total: $${totalPrice.toFixed(2)}`);
+        
+        emptyCart.innerHTML = "<p>Carrinho vazio</p>";
+        counter = {};
+        totalPrice = 0;
+        totalDiv.style.display = "none";
+        cardTitle.innerHTML = "Your Cart (0 items)";
+    
+        if (cartImage) emptyCart.innerHTML += `<img src="your-image-path" class="cart-image">`;
+        if (cartPlaceholder) emptyCart.innerHTML += `<p class="cart-placeholder">Your added items will appear here</p>`;
+      });
+     
     })
   })
 })()
