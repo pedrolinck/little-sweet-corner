7f8a89b63baca0c1ac04f7350f8ef53290f97f83
pedro_linck@outlook.com <pedro_linck@outlook.com>
2025-01-06 23:10:38 -0300

    modifies server route to sync with frontend to load data from json file and adapt content

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ import { fileURLToPath } from 'url';
 
 // create server
 const server = createServer((req, res) => {
-  if (req.url === '/dados' && req.method === 'GET') {
+  if (req.url === '/data' && req.method === 'GET') {
     try {
       const __filename = fileURLToPath(import.meta.url);
       const __dirname = path.dirname(__filename);
-      const filePath = path.join(__dirname, 'dados', 'data.json');
+      const filePath = path.join(__dirname, 'data', 'data.json');
 
       // read json file
       readFile(filePath, 'utf-8', (err, data) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ error: `Erro ao ler o arquivo json: ${err.message}` }));
+          res.end(JSON.stringify({ error: `Error reading JSON file: ${err.message}` }));
         } else {
           res.writeHead(200, { 'Content-Type': 'application/json' });
           // return json content
@@ -24,16 +24,15 @@ const server = createServer((req, res) => {
       });
     } catch (err) {
       res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: `Erro interno do servidor: ${err.message}` }));
+      res.end(JSON.stringify({ error: `Internal error server: ${err.message}` }));
     }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Rota não encontrada');
+    res.end('Route not found');
   }
 });
 
 const PORT = 5500;
 server.listen(PORT, () => {
-  console.log(`Server running in http://127.0.0.1:${PORT}/dados`);
-  // console.log(`Server running in http://localhost:${PORT}/dados`);
+  console.log(`Server running in http://127.0.0.1:${PORT}`);
 });
